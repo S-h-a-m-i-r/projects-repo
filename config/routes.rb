91c6f4f811cbd0cefbadea533b9db37da12c2eae
S-h-a-m-i-r <shahshamu164@gmail.com>
2024-06-19 11:38:03 +0500

    inital commit

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+get 'about', to:'about#index'
+
+# get 'sign_up', to:'registrations#new'
+# post 'sign_up', t0:'registyrations#create'
+
+# get 'sign_in', to: 'sessions#new'
+# post 'sign_in', t0: 'sessions#create'
+
+# delete 'logout', to: 'sessions#destroy'
+
+# root to:'main#index'
+
+
+
+end
